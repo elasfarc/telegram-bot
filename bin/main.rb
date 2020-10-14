12dd9d507362ab796a3965a979055fc9406a62c8
Mohammed Al-Asfar <63879921+elasfarc@users.noreply.github.com>
2020-10-14 09:02:24 +0200

    fix rubocop offenses

--- bin/main.rb
+++ bin/main.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'telegram/bot'
 token = '1015939462:AAH6wJmzjKT9z3AkP0g_3GlsOwVS0fGEEek'
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-   pp message.text
+    pp message.text
   end
 end
--- lib/ccbot.rb
+++ lib/ccbot.rb
@@ -1,5 +1,4 @@
+# frozen_string_literal: true
+
 class CCBot
-    def initialize
-        
-    end
 end
