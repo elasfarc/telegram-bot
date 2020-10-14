c04d1f6c6c6fef02bf40c51662ce4f5a55502dc2
Mohammed Al-Asfar <63879921+elasfarc@users.noreply.github.com>
2020-10-14 10:39:06 +0200

    [CurrencyConverter] ->add constant API
    make it unaccessable outside the class
    test API accessibility outside the class

--- lib/currency_converter.rb
+++ lib/currency_converter.rb
@@ -3,12 +3,12 @@ require 'open-uri'
 require 'json'
 
 class CurrencyConverter
-    API = '4577dd24ef319e6f3fc3'
-    
+  API = '4577dd24ef319e6f3fc3'.freeze
+  private_constant :API
   attr_reader :local_c, :forien_c
-  
 
-  def initialize(local_c, forien_c)
-    @local_c = local_c
-    @forien_c = forien_c
- end
+  def initialize
+    # @local_c = local_c
+    # @forien_c = forien_c
+  end
+end
--- spec/currency_converter_spec.rb
+++ spec/currency_converter_spec.rb
@@ -1,3 +1,13 @@
+require_relative '../lib/currency_converter.rb'
+
 RSpec.describe CurrencyConverter do
-  it
+    let(:currency_converter){CurrencyConverter.new}
+    describe 'constant API' do
+        it 'can not be accessed outside the class' do
+            expect{CurrencyConverter::API}.to raise_error NameError
+            
+        end
+
+    end
+ 
 end
