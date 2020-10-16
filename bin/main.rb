aaffef8d4c48c4669c5cbdb3530a0e904aad6dc6
Mohammed Al-Asfar <63879921+elasfarc@users.noreply.github.com>
2020-10-16 20:38:15 +0200

    /start message

--- bin/main.rb
+++ bin/main.rb
@@ -7,7 +7,11 @@ Telegram::Bot::Client.run(token) do |bot|
     case message.text
     when '/start'
       bot.api.send_message(chat_id: message.chat.id, text: "welcome, #{message.from.first_name}\n")
-      bot.api.send_message(chat_id: message.chat.id, text: " Provide your pair in the form of the following:\n amount from to\n from to \n amount")
+      bot.api.send_message(chat_id: message.chat.id, text: ' Provide your pair in the form of the following:')
+      bot.api.send_message(chat_id: message.chat.id, text: ' [Amount] [currencies pairs]')
+      bot.api.send_message(chat_id: message.chat.id, text: 'currencies pairs = (base currency)(one or more quotes)')
+      bot.api.send_message(chat_id: message.chat.id, text: 'all arguments are optional')
+      bot.api.send_message(chat_id: message.chat.id, text: 'Deafault amount-> 1.0 Deafult BASE->USD')
     else
       input = message.text
       pp input
@@ -17,7 +21,7 @@ Telegram::Bot::Client.run(token) do |bot|
         invalid_input = true
       end
       if invalid_input
-        bot.api.send_message(chat_id: message.chat.id, text: 'check your input')
+        bot.api.send_message(chat_id: message.chat.id, text: 'check the amount again!')
       else
         pp converter.interpreted_input
         text = converter.converter_format
