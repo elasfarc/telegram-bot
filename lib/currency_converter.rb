0bfb03fd09bdc2d92de64a53aaae214e905d16e2
Mohammed Al-Asfar <63879921+elasfarc@users.noreply.github.com>
2020-10-18 12:57:28 +0200

    [ServiceWrap2] -> add CURRENICES list constant
    [CurrencyConverter] -> when creating a new object  assigen @currenices to CURRENICES list constant

--- lib/currency_converter.rb
+++ lib/currency_converter.rb
@@ -1,65 +1,11 @@
-# rubocop:disable Metrics/MethodLength
-# rubocop:disable Layout/LineLength
 require_relative 'service_wrapper_2.rb'
 class CurrencyConverter
   include ServiceWrap2
   attr_reader :pairs, :amount, :currenices, :interpreted_input
 
   def initialize(input)
-    @currenices = {
-      AED: %w[UAE-DIRHAM UNITED-ARAB-EMIRATES],
-      ARS: %w[ARGENTINE-PESO ARGENTINA],
-      AUD: %w[AUSTRALIAN-DOLLAR AUSTRALIA],
-      BGN: %w[BULGARIAN-LEV BULGARIA],
-      BRL: %w[BRAZILIAN-REAL BRAZIL],
-      BSD: %w[BAHAMIAN-DOLLAR BAHAMAS],
-      CAD: %w[CANADIAN-DOLLAR CANADA],
-      CHF: %w[SWISS-FRANC SWITZERLAND],
-      CLP: %w[CHILEAN-PESO CHILE],
-      CNY: %w[CHINESE-RENMINBI CHINA],
-      COP: %w[COLOMBIAN-PESO COLOMBIA],
-      CZK: %w[CZECH-KORUNA CZECH-REPUBLIC],
-      DKK: %w[DANISH-KRONE DENMARK],
-      DOP: %w[DOMINICAN-PESO DOMINICAN-REPUBLIC],
-      EGP: %w[EGYPTIAN-POUND EGYPT],
-      EUR: ['EURO', %w[GERMANY AUSTRIA BELGIUM CYPRUS ESTONIA FINLAND FRANCE GREECE IRELAND ITALY LATVIA LITHUANIA MALTA NETHERLANDS PORTUGAL SLOVAKIA SLOVENIA SPAIN]],
-      FJD: %w[FIJI-DOLLAR FIJI],
-      GBP: %w[POUND-STERLING UNITED-KINGDOM],
-      GTQ: %w[GUATEMALAN-QUETZAL GUATEMALA],
-      HKD: %w[HONG-KONG-DOLLAR HONG-KONG],
-      HRK: %w[CROATIAN-KUNA CROATIA],
-      HUF: %w[HUNGARIAN-FORINT HUNGARY],
-      IDR: %w[INDONESIAN-RUPIAH INDONESIA],
-      ILS: %w[ISRAELI-NEW-SHEKEL ISRAEL],
-      INR: %w[INDIAN-RUPEE INDIA],
-      ISK: %w[ICELANDIC-KRONA ICELAND],
-      JPY: %w[JAPANESE-YEN JAPAN],
-      KRW: %w[SOUTH-KOREAN-WON SOUTH-KOREA],
-      KZT: %w[KAZAKHSTANI-TENGE KAZAKHSTAN],
-      MVR: %w[MALDIVIAN-RUFIYAA MALDIVES],
-      MXN: %w[MEXICAN-PESO MEXICO],
-      MYR: %w[MALAYSIAN-RINGGIT MALAYSIA],
-      NOK: %w[NORWEGIAN-KRONE NORWAY],
-      NZD: %w[NEW-ZEALAND-DOLLAR NEW-ZEALAND],
-      PAB: %w[PANAMANIAN-BALBOA PANAMA],
-      PEN: %w[PERUVIAN-SOL PERU],
-      PHP: %w[PHILIPPINE-PESO PHILIPPINES],
-      PKR: %w[PAKISTANI-RUPEE PAKISTAN],
-      PLN: %w[POLISH-ZLOTY POLAND],
-      PYG: %w[PARAGUAYAN-GUARANI PARAGUAY],
-      RON: %w[ROMANIAN-LEU ROMANIA],
-      RUB: %w[RUSSIAN-RUBLE RUSSIA],
-      SAR: %w[SAUDI-RIYAL SAUDI-ARABIA],
-      SEK: %w[SWEDISH-KRONA SWEDEN],
-      SGD: %w[SINGAPORE-DOLLAR SINGAPORE],
-      THB: %w[THAI-BAHT THAILAND],
-      TRY: %w[TURKISH-LIRA TURKEY],
-      TWD: %w[NEW-TAIWAN-DOLLAR TAIWAN],
-      UAH: %w[UKRAINIAN-HRYVNIA UKRAINE],
-      USD: %w[UNITED-STATES-DOLLAR UNITED-STATES],
-      UYU: %w[URUGUAYAN-PESO URUGUAY],
-      ZAR: %w[SOUTH-AFRICAN-RAND SOUTH-AFRICA]
-    }
+    @currenices = CurrencyConverter::CURRENICES
+
     @pairs = []
     @interpreted_input = input_interpret(input)
   end
@@ -165,5 +111,3 @@ class CurrencyConverter
     pairs.uniq
   end
 end
-# rubocop:enable Metrics/MethodLength
-# rubocop:enable Layout/LineLength
